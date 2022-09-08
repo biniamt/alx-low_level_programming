b8efcf0cc9e57d887e9abbaacca0c701fd44cafa
biniam <biniamtemsgen08@gmail.com>
2022-09-08 14:59:11 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+
+/**                                                                                 
+*main -print the string in the put function                                         
+*                                                                                   
+*description :using the main function                                               
+*this program points "programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar ,but the outcome is a pieace of art ,\n*);
+	return (0);
+}
+
+                 
