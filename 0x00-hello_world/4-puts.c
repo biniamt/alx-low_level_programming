a5389b92d9214e1002671cb5953020305feb0fb5
biniam <biniamtemsgen08@gmail.com>
2022-09-08 14:18:20 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main -print the string in the put function
+*
+*description :using the main function
+*this program points "programming is like building a multi
+*return
+*/int main(void)
+{puts("\programming is like building a multilingual puzzle");
+return(0);
