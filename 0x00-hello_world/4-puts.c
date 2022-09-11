e75c440e944fe58eab346013916489c3b0831c8f
biniam <biniamtemesgen08@gmail.com>
2022-09-11 23:45:13 +0300

    putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#include <stdio.h>                                                                  
-/**                                                                                 
-*main -print the string in the put function                                         
-*                                                                                   
-*description :using the main function                                               
-*this program points "programming is like building a multi                          
-*return : 0                                                                         
-*/                                                                                  
-int main(void)                                                                      
-{                                                                                   
-         puts("\"programming is like building a multilingual puzzle");              
-                                                                                    
-        return (0);
+#include <stdio.h>
+/**
+*main -print the string in the put function
+*
+*description :using the main function
+*this program points "programming is like building a multilingual puzzle
+*return : 0
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
