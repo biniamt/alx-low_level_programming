0b104234a1fd65e0e8444864ad2c440ac25737bd
biniam <biniamtemesgen08@gmail.com>
2022-09-12 23:23:27 +0300

    numb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -prints the numbers since 0 to 9
+*
+*Return:Always (success)
+*/
+int main(void)
+{
+char n;
+for (n='0'; n<='9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return(0);
+}
