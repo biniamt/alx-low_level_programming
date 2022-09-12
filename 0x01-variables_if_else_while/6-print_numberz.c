a33543cd350a8f5f9fbebb5822e7ad78c9380a2d
biniam <biniamtemesgen08@gmail.com>
2022-09-13 00:00:50 +0300

    pzz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main -prints the alphabet at reverse
-*Return:Always (Success)
-*/
+ * main -prints the numbers since 0 to 9
+ * Return:Always(Success)
+ */
 int main(void)
 {
-char c;
-for (c = 'z'; c >= 'a'; c--)
+int n;
+for (n='0'; n <= '9'; n++)
 {
-putchar(c);
+putchar(n);
 }
 putchar('\n');
 return (0);
