2eda73c4a53bf6e0087c50cf0ba71ec005b62cb5
biniam <biniamtemesgen08@gmail.com>
2022-09-12 13:43:10 +0300

    intel

---
+++ 0x00-hello_world/61-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ncurses.h>
+main()
+{
+	clrscr();
+	printf(" shor int is : %2d bytes \n", sizeof(short int));
+	printf(" int is :%2d bytes \n", size of(int));
+	getch();
+}
+
