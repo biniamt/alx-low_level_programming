f91597fcffddbcb4de9037821fddd024e0117448
biniamt <biniamtemesgen08@gmail.com>
2022-12-15 13:36:03 -0800

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>                                                                       
+/**                                                                                      
+ *  * main -causes an infinite loop                                                         
+ *   * return : 0                                                                            
+ *    */                                                                                      
+int main (void)                                                                          
+	        {                                                                                
+			        int i;                                                                           
+				        printf("infinite loop incoming :(\n");                                           
+					        i = 0;                                                                           
+						i = 0;                                                                                   
+						/*while (i<10)                                                                           
+						 *  * {                                                                                     
+						 *   * putchar(i);                                                                           
+						 *    * }                                                                                     
+						 *     */                                                                                      
+						printf(" infinite loop avoided! \\o/\n");                                                
+						return (0);                                                                              
+		}
